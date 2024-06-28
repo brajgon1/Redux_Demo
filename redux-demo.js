cd1fe5bbabca4ccca738e9e12175ddb1d03e824a
Brandon Gonzalez <brajgon@gmail.com>
2024-06-27 19:37:04 -0600

    redux demo

--- redux-demo.js
+++ redux-demo.js
@@ -1,16 +1,27 @@
-const redux = require('redux');
+const redux = require("redux");
 
-const counterReducer = (state, action) => {
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
     return {
-        counter: state.counter + 1
-    }
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
 };
 
-const store = redux.createStore( counterReducer);
+const store = redux.createStore(counterReducer);
 
 const counterSubscriber = () => {
-    const latestState = store.getState();
-    console.log(latestState)
+  const latestState = store.getState();
+  console.log(latestState);
 };
 
 store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
